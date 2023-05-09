abbc79b95cf8f552f5dd0908d43175a9dfafae2e
Angellopz618 <132941162+Angellopz618@users.noreply.github.com>
2023-05-09 13:08:21 -0600

    Update ejemplo1.py

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,14 +1,10 @@
 print('holaaa')
 
 print ('Hola mundo')  
-<<<<<<< HEAD
+
 print('Prueba')
 #Comentario de alejandro 
-=======
+
 print('argentina campeon')
 print('hola')
-<<<<<<< HEAD
 
-=======
->>>>>>> bfb7fee735cf7b09063d05f38c9ebefb71e25382
->>>>>>> a5457d2878087959538de2495573dd32adecfa70
