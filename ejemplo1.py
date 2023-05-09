0d9195fd3ec751286ea67a0c4dc32e588af531dd
Angellopz618 <132941162+Angellopz618@users.noreply.github.com>
2023-05-09 13:07:44 -0600

    a

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,13 +1,6 @@
 print('holaaa')
 
 print ('Hola mundo')  
-<<<<<<< HEAD
-
-
-=======
 print('argentina campeon')
-<<<<<<< HEAD
->>>>>>> 36c26a62c8d62a9d992f23c8412ad646add37232
-=======
 print('hola')
->>>>>>> bfb7fee735cf7b09063d05f38c9ebefb71e25382
+
